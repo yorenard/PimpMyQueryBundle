ccba829a7530e9160c8453380deb4d03dc5429a0
Yoann RENARD <yrenard@lafourchette.com>
2014-04-11 17:41:05 +0200

    fix configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('yorenard_pmq');
 
         return $treeBuilder;
     }
